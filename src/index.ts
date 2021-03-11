e28841ecf2cffa8ba4ea2c1ec35085433b6b4d41
Tony <tony.js@zoho.eu>
2021-03-11 16:20:40 -0300

    feat: add support to action data compose

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import pick from '@cookiex/dot-object/dist/pick'
 
+
 export interface AnyAction {
   type: any
   data?: any
@@ -26,27 +27,47 @@ const combineReducers = <T, A>(
   }
 }
 
-type Configuration<T, A> =
-  & { [K in keyof A]?: ( state: T, data: A[K] ) => T }
-  & (
-    | { [DEFAULT]?: ( state: T ) => T }
-    | { [COMPOSE]?: T extends object ? { [K in keyof T]?: Configuration<T[K], A> } : undefined }
-    | { [COMPOSE_NULLISH]?: T extends object ? { [K in keyof T]?: Configuration<T[K], A> } : undefined }
+type CompositeConfiguration<T, A, Act extends AnyAction> = T extends object
+  ? {
+    [K in keyof T]?: Configuration<T[K], A, Act, K extends keyof A ? K : null>
+  }
+  : undefined | null
+
+type KeyOfConfiguration<T, A, Act extends AnyAction, C extends keyof A | null = null> =
+  keyof (
+    C extends keyof A
+      ? { [K in keyof A[C]]?: ( state: T, data: A[C][K] ) => T }
+      : { [K in keyof A]?: ( state: T, data: A[K] ) => T }
   )
+type Configuration<T, A, Act extends AnyAction, C extends keyof A | null = null> =
+  & (
+    C extends keyof A
+      ? { [K in keyof A[C]]?: ( state: T, data: A[C][K] ) => T }
+      : { [K in keyof A]?: ( state: T, data: A[K] ) => T }
+  )
+  & ( {
+      [DEFAULT]?: ( state: T, action: Act ) => T
+      [COMPOSE]?: CompositeConfiguration<T, A, Act>
+    } | {
+      [DEFAULT]?: ( state: T, action: Act ) => T
+      [COMPOSE_NULLISH]?: CompositeConfiguration<T, A, Act>
+    } )
 
 interface createReducer {
   <T, A, Act extends AnyAction = AnyAction>(
-    config: Configuration<T, A>
+    config: Configuration<T, A, Act>
   ): ( state: T, action: Act ) => T
 }
 
+const extractKeys = <T>( data: T ): ( keyof T )[] => [
+  ...Object.getOwnPropertyNames( data ),
+  ...Object.getOwnPropertySymbols( data ),
+] as ( keyof T )[]
+
 const createReducer: createReducer = <T, A, Act extends AnyAction = AnyAction>(
-  config: Configuration<T, A>
+  config: Configuration<T, A, Act>
 ) => {
-  const keys = [
-    ...Object.getOwnPropertyNames( config ),
-    ...Object.getOwnPropertySymbols( config )
-  ].filter( privateFilter ) as ( keyof A )[]
+  const keys = extractKeys( config ).filter( privateFilter ) as ( KeyOfConfiguration<T, A, Act> )[]
 
   const composeIsNullish = COMPOSE_NULLISH in config
 
@@ -79,7 +100,7 @@ const createReducer: createReducer = <T, A, Act extends AnyAction = AnyAction>(
 
     if ( COMPOSE in config ) return combine( state, action )
 
-    if ( DEFAULT in config ) return config[DEFAULT]( state )
+    if ( DEFAULT in config ) return config[DEFAULT]( state, action )
 
     return ( state ?? {} ) as T
   }
@@ -97,6 +118,7 @@ export const DEFAULT = Symbol( 'store-reducer-default' )
 
 const privateKeys = [ COMPOSE, DEFAULT ]
 
-const privateFilter = ( key: string | symbol ) => !privateKeys.includes( key as symbol )
+const privateFilter = <K extends keyof any>( key: K ): key is Exclude<K, typeof COMPOSE | typeof DEFAULT | typeof COMPOSE_NULLISH>  =>
+  !privateKeys.includes( key as symbol )
 
 export default createReducer
